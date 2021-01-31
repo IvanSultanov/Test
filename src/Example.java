6c0b6f9114f7de8d8f92270d86c0f10ad94ec3e9
IvanSultanov <sultanovivan229886@gmail.com>
2021-02-01 00:13:22 +0600

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main (String[] Args) {
+        boolean b;
+        double radius = 0.225, height = 20, volume;
+        volume = Math.PI * radius * radius * height;
+        System.out.println("Объем цилиндра равен " + volume);
+    }
+}
